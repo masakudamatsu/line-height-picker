a9793efad758929ca90e826177c24f5dbafbe1e9
Unknown <m.kudamatsu@gmail.com>
2020-03-16 20:24:04 +0900

    Green for e2e: Render the SVG logo #11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyle from './theme/GlobalStyle';
 import Title from './components/Title';
+import Logo from './components/Logo';
 function App() {
   return (
     <>
       <GlobalStyle />
       <Title />
+      <Logo />
     </>
   );
 }
