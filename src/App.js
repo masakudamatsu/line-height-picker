bc90b1f1dd46033b97148827805ea7f98143cc59
Unknown <m.kudamatsu@gmail.com>
2020-03-16 12:45:52 +0900

    CSS Reset with Styled Components #8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-
+import React from 'react';
+import GlobalStyle from './theme/GlobalStyle';
 function App() {
   return (
     <>
-      <h1>Hello, world!</h1>
+      <GlobalStyle />
     </>
   );
 }
