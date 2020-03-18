6ecabca0fce95f8a37ed0d793e38839a9f7b16b4
Unknown <m.kudamatsu@gmail.com>
2020-03-18 11:36:14 +0900

    Template react component JS file

---
+++ src/components/template.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {STYLEDCOMPONENT} from '../theme/style';
+
+const COMPONENT = () => {
+  return <STYLEDCOMPONENT></STYLEDCOMPONENT>;
+};
+
+export default COMPONENT;
