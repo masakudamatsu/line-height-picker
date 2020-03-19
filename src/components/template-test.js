6b6c5c50019eeb12b3605f7182661d9cc52c1fec
Unknown <m.kudamatsu@gmail.com>
2020-03-19 15:58:04 +0900

    Update the template test code #22

--- src/components/template-test.js
+++ src/components/template-test.js
@@ -1,5 +1,6 @@
 // import React from 'react';
-// import {render, cleanup} from '@testing-library/react';
+// import render from './test-utils/render';
+// import {cleanup} from '@testing-library/react';
 // import '@testing-library/jest-dom/extend-expect';
 // import 'jest-styled-components';
 // import {axe} from 'jest-axe';
