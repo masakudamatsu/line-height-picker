4d152733a761f602068b86f6d57454633d76f1a1
Unknown <m.kudamatsu@gmail.com>
2020-03-19 16:19:24 +0900

    Red for e2e: Test if 404 page guides user to landing page #23

--- cypress/e2e/landingPage.js
+++ cypress/e2e/landingPage.js
@@ -22,4 +22,14 @@ describe('The 404 Page', () => {
     cy.visit('/random-text');
     cy.findByText(/404/).should('exist');
   });
+  it('guides the user to the landing page', () => {
+    cy.visit('/random-text');
+    cy.findByText(/click/i).click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`);
+    cy.findByTitle(/logo/i).should('exist');
+    cy.findByTestId('description').should('exist');
+    cy.findByText(/upload/i).should('exist');
+    cy.findByText(/demo/i).should('exist');
+    cy.findByTestId('footer').should('exist');
+  });
 });
