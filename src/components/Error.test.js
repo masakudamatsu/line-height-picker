baa4bdadc550b19c523530f0f34945c615acb6f7
Unknown <m.kudamatsu@gmail.com>
2020-03-19 16:12:11 +0900

    Red: Write a component test for Error #23

---
+++ src/components/Error.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import render from './test-utils/render';
+import {cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components';
+import {axe} from 'jest-axe';
+import 'jest-axe/extend-expect';
+
+import Error from './Error';
+
+test('renders correctly', () => {
+  const {container} = render(<Error />);
+  expect(container).toMatchInlineSnapshot();
+});
+
+test('is accessible', async () => {
+  const {container} = render(<Error />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+  cleanup();
+});
