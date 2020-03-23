2945a5354e43f677f3ceeab147b535e03f2b7511
Unknown <m.kudamatsu@gmail.com>
2020-03-23 17:21:21 +0900

    Green: Render ChangeFontButton on x-height page #24

--- src/components/Xheight.js
+++ src/components/Xheight.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import XheightBox from './XheightBox';
 import FontNameDisplay from './FontNameDisplay';
+import {ButtonContainer} from '../theme/style';
+import ChangeFontButton from './ChangeFontButton';
 
 const Xheight = () => {
   return (
@@ -10,6 +12,9 @@ const Xheight = () => {
       <main>
         <XheightBox />
         <FontNameDisplay />
+        <ButtonContainer>
+          <ChangeFontButton />
+        </ButtonContainer>
       </main>
     </>
   );
