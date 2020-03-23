f2619294e6ee5737bf8fccf8d1d96ed74a0449e6
Unknown <m.kudamatsu@gmail.com>
2020-03-23 16:45:00 +0900

    Green: Render the FontNameDisplay component on x-height page #24

--- src/components/Xheight.js
+++ src/components/Xheight.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import XheightBox from './XheightBox';
+import FontNameDisplay from './FontNameDisplay';
 
 const Xheight = () => {
   return (
@@ -8,6 +9,7 @@ const Xheight = () => {
       <Header stepNow={2} />
       <main>
         <XheightBox />
+        <FontNameDisplay />
       </main>
     </>
   );
