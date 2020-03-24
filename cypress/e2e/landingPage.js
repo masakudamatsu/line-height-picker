2a803b1544750947d915526b473aec1c63a0a7dc
Unknown <m.kudamatsu@gmail.com>
2020-03-24 14:19:09 +0900

    Red: Test if the modular scale boxes are shown on modular scale page #37

--- cypress/e2e/landingPage.js
+++ cypress/e2e/landingPage.js
@@ -43,6 +43,10 @@ describe('Modular Scale Page', () => {
   it('shows the UI components correctly', () => {
     cy.visit('/modular-scale');
     cy.checkHeaderFooterRendering();
+    cy.findByText(/pick modular scale/i).should('exist');
+    cy.findByText(/set the ratio of/).should('exist');
+    cy.findByLabelText(/x-height/i).should('exist');
+    cy.findByLabelText(/line-height/i).should('exist');
     cy.findByTestId('FontNameDisplay').should('exist');
     cy.findByText(/change font/i).should('exist');
     cy.findByTestId('XheightDisplay').should('exist');
