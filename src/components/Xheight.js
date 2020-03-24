a76fcf8e33df2025f8eb61134d19be55e9141b0d
Unknown <m.kudamatsu@gmail.com>
2020-03-24 09:38:24 +0900

    Green: Add the link button to modular scale page on x-height page #24

--- src/components/Xheight.js
+++ src/components/Xheight.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Header from './Header';
 import XheightBox from './XheightBox';
 import FontNameDisplay from './FontNameDisplay';
-import {ButtonContainer} from '../theme/style';
+import {Button, ButtonContainer, NoWrap} from '../theme/style';
 import ChangeFontButton from './ChangeFontButton';
+import {Link} from 'react-router-dom';
 
 const Xheight = () => {
   return (
@@ -14,6 +15,12 @@ const Xheight = () => {
         <FontNameDisplay />
         <ButtonContainer>
           <ChangeFontButton />
+          <Button as={Link} to="/modular-scale">
+            <NoWrap>Pick</NoWrap>
+            <NoWrap>Modular</NoWrap>
+            <NoWrap>Scale</NoWrap>
+            <NoWrap>→</NoWrap>
+          </Button>
         </ButtonContainer>
       </main>
     </>
