d11ded7d1db98ea1d0e29e6e516262b5371874f1
Unknown <m.kudamatsu@gmail.com>
2020-03-25 20:10:35 +0900

    Revise the sample paragraphs #40

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -23,14 +23,9 @@ const Preview = () => {
         <PageTitle>Preview sample paragraphs</PageTitle>
         <SampleParagraphs>
           <p data-testid="sampleParagraph1">
-            Reed College at that time offered perhaps the best calligraphy
-            instruction in the country. Throughout the campus every poster,
-            every label on every drawer, was beautifully hand calligraphed.
-            Because I had dropped out and didn’t have to take the normal
-            classes, I decided to take a calligraphy class to learn how to do
-            this. I learned about serif and sans serif typefaces, about varying
-            the amount of space between different letter combinations, about
-            what makes great typography great. It was beautiful, historical,
+            … I learned about serif and sans serif typefaces, about varying the
+            amount of space between different letter combinations, about what
+            makes great typography great. It was beautiful, historical,
             artistically subtle in a way that science can’t capture, and I found
             it fascinating.
           </p>
@@ -44,10 +39,7 @@ const Preview = () => {
             And since Windows just copied the Mac, it’s likely that no personal
             computer would have them. If I had never dropped out, I would have
             never dropped in on this calligraphy class, and personal computers
-            might not have the wonderful typography that they do. Of course it
-            was impossible to connect the dots looking forward when I was in
-            college. But it was very, very clear looking backward 10 years
-            later.
+            might not have the wonderful typography that they do. …
           </p>
         </SampleParagraphs>
         <ParagraphOneRemRightAligned>
