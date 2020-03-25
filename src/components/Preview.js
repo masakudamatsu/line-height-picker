468fe3aeba9b54945bcd0231c622abfe65a9700a
Unknown <m.kudamatsu@gmail.com>
2020-03-25 16:25:50 +0900

    Green: Show the page title on Preview page #40

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import {FormTitle} from '../theme/style';
 
 const Preview = () => {
   return (
     <>
       <Header stepNow={4} />
-      <main></main>
+      <main>
+        <FormTitle>Preview sample paragraphs</FormTitle>
+      </main>
     </>
   );
 };
