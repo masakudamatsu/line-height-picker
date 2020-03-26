8019354d305b720bc271858c7b6d7342498da5b9
Unknown <m.kudamatsu@gmail.com>
2020-03-27 08:47:25 +0900

    Red e2e: Test if demo shows font-size in response to x-height value #49

--- cypress/e2e/demo.js
+++ cypress/e2e/demo.js
@@ -2,13 +2,26 @@ describe('Demo', () => {
   it('shows the CSS code correctly', () => {
     const fontFamily = `font-family: 'Open Sans'`;
     const fontWeight = `font-weight: 400`;
+    const userData = {
+      xHeight: 10,
+    };
+    const OpenSansFontMetrics = {
+      unitsPerEm: 2048,
+      sxHeight: 1096,
+    };
+    const fontSize = `font-size: ${(
+      (userData.xHeight * OpenSansFontMetrics.unitsPerEm) /
+      OpenSansFontMetrics.sxHeight
+    ).toFixed(4)}px`;
     cy.visit('/');
     cy.findByText(/demo/i).click();
+    cy.findByLabelText(/x-height/i).type(userData.xHeight);
     cy.findByText(/scale/i).click();
     cy.findByText(/preview/i).click();
     cy.findByText(/css/i).click();
     cy.findByTestId('cssCode')
       .contains(fontFamily)
-      .contains(fontWeight);
+      .contains(fontWeight)
+      .contains(fontSize);
   });
 });
