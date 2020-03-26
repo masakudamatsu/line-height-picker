1aae20464667205debb1640c88b2c18419fabf77
Unknown <m.kudamatsu@gmail.com>
2020-03-26 10:21:11 +0900

    Green: Add the page title to CSS page #45

--- src/components/GetCSS.js
+++ src/components/GetCSS.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import Header from './Header';
-import {Button, NoWrap} from '../theme/style';
+import {Button, NoWrap, PageTitle} from '../theme/style';
 
 const GetCSS = () => {
   return (
     <>
       <Header stepNow={5} />
       <main>
+        <PageTitle>Get CSS</PageTitle>
         <Button as={Link} to="/preview">
           Back to preview
           <NoWrap>←</NoWrap>
