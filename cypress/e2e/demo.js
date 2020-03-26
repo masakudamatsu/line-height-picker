88853ba9a28658e277c5fcdccb68808975c9d641
Unknown <m.kudamatsu@gmail.com>
2020-03-26 17:31:52 +0900

    Red e2e: Test if demo shows font-weight 400 on CSS page #49

--- cypress/e2e/demo.js
+++ cypress/e2e/demo.js
@@ -1,11 +1,14 @@
 describe('Demo', () => {
   it('shows the CSS code correctly', () => {
     const fontFamily = `font-family: 'Open Sans'`;
+    const fontWeight = `font-weight: 400`;
     cy.visit('/');
     cy.findByText(/demo/i).click();
     cy.findByText(/scale/i).click();
     cy.findByText(/preview/i).click();
     cy.findByText(/css/i).click();
-    cy.findByTestId('cssCode').contains(fontFamily);
+    cy.findByTestId('cssCode')
+      .contains(fontFamily)
+      .contains(fontWeight);
   });
 });
