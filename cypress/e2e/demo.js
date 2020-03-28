9f271fd394221457cb7e058674465218c0d06848
Unknown <m.kudamatsu@gmail.com>
2020-03-28 12:30:07 +0900

    Red e2e: Test if demo shows line-height in response to modular scale #49

--- cypress/e2e/demo.js
+++ cypress/e2e/demo.js
@@ -1,27 +1,42 @@
 describe('Demo', () => {
   it('shows the CSS code correctly', () => {
+    // setup
     const fontFamily = `font-family: 'Open Sans'`;
     const fontWeight = `font-weight: 400`;
     const userData = {
       xHeight: 10,
+      xHeightRatio: 1,
+      lineHeightRatio: 3,
     };
     const OpenSansFontMetrics = {
       unitsPerEm: 2048,
       sxHeight: 1096,
     };
-    const fontSize = `font-size: ${(
-      (userData.xHeight * OpenSansFontMetrics.unitsPerEm) /
-      OpenSansFontMetrics.sxHeight
-    ).toFixed(4)}px`;
+    const fontSizeValue = (
+      userData.xHeight *
+      (OpenSansFontMetrics.unitsPerEm / OpenSansFontMetrics.sxHeight)
+    ).toFixed(4);
+    const fontSize = `font-size: ${fontSizeValue}px`;
+    const lineHeight = `line-height: ${(
+      (userData.xHeight * (userData.lineHeightRatio / userData.xHeightRatio)) /
+      fontSizeValue
+    ).toFixed(4)}`;
+    // execute
     cy.visit('/');
     cy.findByText(/demo/i).click();
     cy.findByLabelText(/x-height/i).type(userData.xHeight);
     cy.findByText(/scale/i).click();
+    cy.findByLabelText(/x-height/i).type(userData.xHeightRatio);
+    cy.findByLabelText(/line-height/i, {selector: 'input'}).type(
+      userData.lineHeightRatio,
+    );
     cy.findByText(/preview/i).click();
     cy.findByText(/css/i).click();
+    // verify
     cy.findByTestId('cssCode')
       .contains(fontFamily)
       .contains(fontWeight)
-      .contains(fontSize);
+      .contains(fontSize)
+      .contains(lineHeight);
   });
 });
