1c7b0d1c6a381b0d8ac1a61489951029db29df44
Unknown <m.kudamatsu@gmail.com>
2020-03-31 16:04:55 +0900

    Green: Call Redirect upon font file uploading #57

--- src/components/FontFileUploader.js
+++ src/components/FontFileUploader.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {Redirect} from 'react-router';
 import {Button, NoWrap} from '../theme/style';
 
 const FontFileUploader = props => {
+  const [redirect, setRedirect] = React.useState(false);
   const handleClick = event => {
     document.getElementById('hiddenFileInput').click();
   };
   const handleChange = event => {
     const fileUploaded = event.target.files[0];
     props.handleFontFile(fileUploaded);
+    setRedirect(true);
   };
+  if (redirect) {
+    return <Redirect push to="/x-height" />;
+    // The push attribute keeps the browser history, instead of overriding, so the user can click the Back button in the browser to be back to the landing page. See https://reacttraining.com/react-router/web/api/Redirect/push-bool
+  }
   return (
     <>
       <Button onClick={handleClick}>
