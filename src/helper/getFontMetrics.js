98a758f53b64a527ced56fa8e05ead2e3ceb3206
Unknown <m.kudamatsu@gmail.com>
2020-03-31 16:12:07 +0900

    Green: Uploading font file jumps to x-height page & shows font name #57

---
+++ src/helper/getFontMetrics.js
@@ -0,0 +1,18 @@
+const getFontMetrics = (font) => {
+  const fontMetrics = {};
+
+  const namingTable = font.tables['name'];
+  fontMetrics.fontFamily = namingTable['fontFamily'].en;
+  fontMetrics.fullName = namingTable['fullName'].en;
+
+  const headTable = font.tables['head'];
+  fontMetrics.unitsPerEm = headTable['unitsPerEm'];
+
+  const os2Table = font.tables['os2'];
+  fontMetrics.usWeightClass = os2Table['usWeightClass'];
+  fontMetrics.sxHeight = os2Table['sxHeight'];
+
+  return fontMetrics;
+};
+
+export default getFontMetrics;
