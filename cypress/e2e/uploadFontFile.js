6af0fe6dece930c21719a24e8b0c63feb52469d8
Unknown <m.kudamatsu@gmail.com>
2020-04-01 20:51:42 +0900

    Refactor e2e test for uploading font file on landing page #57
    Use Roboto Slab to see if the font name does change. Fix the error in the attachFile(): binary file needs to be explicitly declared as the optional argument.

--- cypress/e2e/uploadFontFile.js
+++ cypress/e2e/uploadFontFile.js
@@ -3,15 +3,17 @@ describe('Uploading a font file on landing page', () => {
     // This test ends up with an error message in Cypress, but the test does pass.
 
     // Setup
-    const fontFileName = 'OpenSans-Regular.ttf';
-    const expectedFontName = 'Open Sans';
+    const fontFileName = 'RobotoSlab-Light.ttf';
+    const expectedFontName = 'Roboto Slab';
 
     // Execute
     cy.visit('/');
     cy.findByText(/upload/i).click(); // Just to make sure that the user can find and then click the upload button. This command does not launch the file upload dialog box in Cypress. So we need the next command:
     cy.findByTestId('hiddenFileInput').attachFile({
       filePath: fontFileName,
-    }); // This command does not exactly reflect how the user interacts with our UI. But there's no other way to simulate it. And it causes an error message from opentype.js for some reason.
+      mimeType: 'font/ttf',
+      encoding: 'binary',
+    }); // This command does not exactly reflect how the user interacts with our UI. But there's no other way to simulate it.
 
     // Verify
     cy.url().should('eq', `${Cypress.config().baseUrl}/x-height`);
