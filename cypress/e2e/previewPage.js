11afd3f6294ed5769edb16e1708cd80ed13215c1
Unknown <m.kudamatsu@gmail.com>
2020-04-02 18:09:58 +0900

    Refactor e2e test for Preivew page #70
    Also add tests for changing x-height value

--- cypress/e2e/previewPage.js
+++ cypress/e2e/previewPage.js
@@ -1,6 +1,27 @@
+const userData = {
+  xHeight: 10,
+  xHeightRatio: 1,
+  lineHeightRatio: 3,
+};
+
 describe('Preview Page in demo', () => {
+  // setup
+  const fontFamily = `Open Sans`;
+  const fontWeight = `400`;
+  const OpenSansFontMetrics = {
+    unitsPerEm: 2048,
+    sxHeight: 1096,
+  };
+
   beforeEach(() => {
-    cy.visit('/preview');
+    cy.visit('/x-height');
+    cy.findByLabelText(/x-height/i).type(userData.xHeight);
+    cy.findByText(/scale/i).click();
+    cy.findByLabelText(/x-height/i).type(userData.xHeightRatio);
+    cy.findByLabelText(/line-height/i, {selector: 'input'}).type(
+      userData.lineHeightRatio,
+    );
+    cy.findByText(/preview/i).click();
   });
 
   it('shows the UI components correctly', () => {
@@ -11,8 +32,12 @@ describe('Preview Page in demo', () => {
     cy.findByText(/excerpt/i).should('exist');
     cy.findByTestId('FontNameDisplay').should('exist');
     cy.findByText(/change font/i).should('exist');
-    cy.findAllByLabelText(/x-height/i).should('exist');
     cy.findByLabelText(/line-height/i, {selector: 'input'}).should('exist');
+  });
+
+  it('takes the user to the CSS page after clicking the button for it', () => {
+    cy.findByText(/css/i).click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}/css`);
   });
 
   it('allows the user to change font by clicking the "change font" button', () => {
@@ -29,20 +54,44 @@ describe('Preview Page in demo', () => {
     cy.assertFontNameFromPreviewPageOn(expectedFontName, expectedFontWeight);
   });
 
-  it('takes the user to the CSS page after clicking the button for it', () => {
-    cy.findByText(/css/i).click();
-    cy.url().should('eq', `${Cypress.config().baseUrl}/css`);
+  it('allows the user to change x-height, which will be shown immediately and used to calculate font-size', () => {
+    const newUserData = {
+      xHeight: 12,
+    };
+    // execute
+    cy.findByTestId('x-height-in-pixel')
+      .clear()
+      .type(newUserData.xHeight);
+    // verify
+    cy.assertXheightFontSizeFromPreviewPageOn(
+      newUserData.xHeight,
+      OpenSansFontMetrics,
+    );
   });
 });
 
 describe('Preview Page after uploading a font file', () => {
+  // setup
+  const fontFamily = `Roboto Slab`;
+  const fontWeight = `300`;
+  const RobotoSlabFontMetrics = {
+    unitsPerEm: 2048,
+    sxHeight: 1082,
+  };
+
   beforeEach(() => {
     const fontFileName = 'RobotoSlab-Light.ttf';
     cy.visit('/');
     cy.upload('hiddenFileInput', fontFileName); // see support/commands.js
+    cy.findByLabelText(/x-height/i).type(userData.xHeight);
     cy.findByText(/scale/i).click();
+    cy.findByLabelText(/x-height/i).type(userData.xHeightRatio);
+    cy.findByLabelText(/line-height/i, {selector: 'input'}).type(
+      userData.lineHeightRatio,
+    );
     cy.findByText(/preview/i).click();
   });
+
   it('allows the user to change font by clicking the "change font" button', () => {
     // Setup
     const fontFileName = 'OpenSans-Regular.ttf';
@@ -55,5 +104,20 @@ describe('Preview Page after uploading a font file', () => {
 
     // verify
     cy.assertFontNameFromPreviewPageOn(expectedFontName, expectedFontWeight);
+  });
+
+  it('allows the user to change x-height, which will be shown immediately and used to calculate font-size', () => {
+    const newUserData = {
+      xHeight: 13,
+    };
+    // execute
+    cy.findByTestId('x-height-in-pixel')
+      .clear()
+      .type(newUserData.xHeight);
+    // verify
+    cy.assertXheightFontSizeFromPreviewPageOn(
+      newUserData.xHeight,
+      RobotoSlabFontMetrics,
+    );
   });
 });
