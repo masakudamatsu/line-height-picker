9645de998dd9d462a68ac9041713abd1e1627d9a
Unknown <m.kudamatsu@gmail.com>
2020-04-09 19:20:45 +0900

    Convert error messages into JSX #86
    Otherwise the link text does not work.

--- src/helper/errorMessages.js
+++ src/helper/errorMessages.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import {ExternalLink} from '../theme/style';
 export const fileExtensionError = {
-  whatHappened:
-    'Our webapp only works with the font files whose name ends with .otf, .ttf, or .woff.',
-  howToResolve:
-    'Please select an OpenType Font file (ending with .otf), TrueType Font file (.ttf), or Web Open Font Format file (.woff).',
-  extraText:
-    'For more detail on font files, we recommend “<a href="https://www.suttle-straus.com/blog/a-simple-guide-to-font-file-types">A Simple Guide To Font File Types</a>”, written by Amy Olson in 2017.',
+  whatHappened: (
+    <>
+      Our webapp only works with the font files whose name ends with .otf, .ttf,
+      or .woff.
+    </>
+  ),
+  howToResolve: (
+    <>
+      Please select an OpenType Font file (ending with .otf), TrueType Font file
+      (.ttf), or Web Open Font Format file (.woff).
+    </>
+  ),
+  extraText: (
+    <>
+      For more detail on font files, have a look at{' '}
+      <ExternalLink href="https://www.suttle-straus.com/blog/a-simple-guide-to-font-file-types">
+        “A Simple Guide To Font File Types”
+      </ExternalLink>
+      , written by Amy Olson in 2017.
+    </>
+  ),
 };
