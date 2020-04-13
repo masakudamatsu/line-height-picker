fcaf8f176131ed31eaeb9bb35ebc16c639833836
Unknown <m.kudamatsu@gmail.com>
2020-04-13 12:09:01 +0900

    Refactor e2e tests for 404 page #23
    We do not repeat the UI component assertions for landing page.

--- cypress/e2e/the404Page.js
+++ cypress/e2e/the404Page.js
@@ -11,11 +11,6 @@ describe('The 404 Page', () => {
   it('guides the user to the landing page', () => {
     cy.findByText(/click/i).click();
     cy.url().should('eq', `${Cypress.config().baseUrl}/`);
-    cy.findByTitle(/logo/i).should('exist');
-    cy.findByTestId('description').should('exist');
-    cy.findByText(/upload/i).should('exist');
-    cy.findByText(/demo/i).should('exist');
-    cy.findByTestId('footer').should('exist');
   });
   it('shows other UI components', () => {
     cy.findByTestId('footer').should('exist');
