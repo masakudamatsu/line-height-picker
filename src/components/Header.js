ca25edacedcf7ae912785e5c952cc69616ed8fd5
Unknown <m.kudamatsu@gmail.com>
2020-04-15 13:16:21 +0900

    Replace string numbers with numbers in curly brackets as props #26
    Otherwise the console shows warning messages.

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,38 +11,38 @@ import {
 } from '../theme/style';
 
 const Header = props => {
-  let step1 = <Step number="1" />;
-  let step2 = <Step number="2" />;
-  let step3 = <Step number="3" />;
-  let step4 = <Step number="4" />;
-  let step5 = <Step number="5" />;
+  let step1 = <Step number={1} />;
+  let step2 = <Step number={2} />;
+  let step3 = <Step number={3} />;
+  let step4 = <Step number={4} />;
+  let step5 = <Step number={5} />;
   if (props.stepNow > 1) {
-    step1 = <Step number="1" done />;
+    step1 = <Step number={1} done />;
   }
   if (props.stepNow > 2) {
-    step2 = <Step number="2" done />;
+    step2 = <Step number={2} done />;
   }
   if (props.stepNow > 3) {
-    step3 = <Step number="3" done />;
+    step3 = <Step number={3} done />;
   }
   if (props.stepNow > 4) {
-    step4 = <Step number="4" done />;
+    step4 = <Step number={4} done />;
   }
   switch (props.stepNow) {
     case 1:
-      step1 = <Step number="1" now />;
+      step1 = <Step number={1} now />;
       break;
     case 2:
-      step2 = <Step number="2" now />;
+      step2 = <Step number={2} now />;
       break;
     case 3:
-      step3 = <Step number="3" now />;
+      step3 = <Step number={3} now />;
       break;
     case 4:
-      step4 = <Step number="4" now />;
+      step4 = <Step number={4} now />;
       break;
     case 5:
-      step5 = <Step number="5" now />;
+      step5 = <Step number={5} now />;
       break;
     default:
       break;
