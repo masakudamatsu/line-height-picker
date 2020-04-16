3e0d17a6a7ea2790a74e38dbf549f4eb0f351003
Unknown <m.kudamatsu@gmail.com>
2020-04-16 12:58:52 +0900

    Remove the redundant line of code #39
    Importing Link component without using it gives the failed proptypes warning in the console.

--- src/components/Step.js
+++ src/components/Step.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {StepNumber, StepNumberBox} from '../theme/style';
-import {Link} from 'react-router-dom';
 
 const Step = props => {
   let internalURL;
