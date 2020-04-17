896ce32e4661fd1c0927b7cb41fa4c297df39288
Unknown <m.kudamatsu@gmail.com>
2020-04-17 09:57:39 +0900

    Add e2e test for nav bar on preview page #39

--- cypress/e2e/previewPage.js
+++ cypress/e2e/previewPage.js
@@ -369,4 +369,13 @@ describe('Preview page: Navigation bar', () => {
     cy.findByText('5').click();
     cy.url().should('eq', `${Cypress.config().baseUrl}/preview`);
   });
+  it.only('DOES take the user to the get CSS page after clicking number 5 in the header, if the user has already visited', () => {
+    // set up
+    cy.findByText(/css/i).click();
+    cy.findByText('4').click();
+    // execute
+    cy.findByText('5').click();
+    // verify
+    cy.url().should('eq', `${Cypress.config().baseUrl}/css`);
+  });
 });
