f0e0e1255199ad01def34773bfda9f6906d57673
Unknown <m.kudamatsu@gmail.com>
2020-05-23 16:56:52 +0900

    Revise footer text #19

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,18 @@ import {ExternalLink, FooterWrapper} from '../theme/style';
 const Footer = props => {
   return (
     <FooterWrapper as="footer" data-testid="footer" page404={props.page404}>
-      <p>
-        Designed and Coded by Masa Kudamatsu in 2020 (
-        <ExternalLink href="https://github.com/masakudamatsu/line-height-picker">
-          SOURCE CODE
-        </ExternalLink>
-        ).
-      </p>
+      <p>Designed and Coded by Masa Kudamatsu in 2020.</p>
       <p>
         Powered by{' '}
         <ExternalLink href="https://opentype.js.org/">
           Opentype.js.
         </ExternalLink>
+      </p>
+      <p>
+        <ExternalLink href="https://github.com/masakudamatsu/line-height-picker">
+          The source code is available at GitHub
+        </ExternalLink>
+        .
       </p>
     </FooterWrapper>
   );
