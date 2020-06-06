0bc738052b212ce11c48aa3e3a51e5a4d5037b27
Unknown <m.kudamatsu@gmail.com>
2020-06-06 13:36:33 +0900

    Revise the color scheme to meet the contrast ratio guidelines #13

--- src/theme/colorPalette.js
+++ src/theme/colorPalette.js
@@ -1,43 +1,46 @@
-const darkest = 'hsl(0, 0%, 15%)';
-const darkerThanDarkest = 'hsl(0, 0%, 25%)';
-const disabled = 'hsl(0, 0%, 35%)'; // For disabled links on header navigation
-const middleDark = 'hsl(0, 0%, 36%)';
-const middle = 'hsl(0, 0%, 46%)'; // 1:4.5 contrast ratio to both pure black and pure white
-const footerText = 'hsl(0, 0%, 67%)';
-const visitedLink = 'hsl(0, 0%, 76%)';
+const backgroundDark = 'hsl(0, 0%, 10%)';
+const figureDark = 'hsl(0, 0%, 51%)'; // 1 to 4.5 contrast ratio to backgroundDark
+const backgroundLight = 'hsl(0, 0%, 40%)'; // 1 to 3 contrast ratio to the backgroundDark
+const alert = 'hsl(60,80%,60%)'; // 1 to 4.5 contrast ratio with backgroundLight
+const figureLight = 'hsl(0, 0%, 90%)'; // 1 to 4.5 contrast ratio with backgroundLight
+const backgroundLightest = 'hsl(0, 0%, 51%)'; // 1 to 3 contrast ratio to the figureLight for button's focus-state color
+
 const opacityForDisabledText = '0.35'; // For disabled text for navigation and buttons
-const pureBlack = 'hsl(0, 0%, 0%)';
-const alert = 'hsl(335, 71%, 64%)';
+
 const light = {
   source: 'hsla(0, 0%, 100%, 0.9)',
-  reflection: 'hsla(0, 0%, 100%, 0.5)',
+  reflection: 'hsla(0, 0%, 100%, 0.56)', // 1 to 3 contrast ratio with backgroundLight (button shadow against control panel)
 };
 
+// For link text
+const middle = 'hsl(0, 0%, 74%)'; // 1 to 3 contrast ratio to backgroundLight
+const middleDark = 'hsl(0, 0%, 36%)';
+const visitedLink = 'hsl(180,70%,89%)'; // 1 to 5 contrast ratio with second darkest color
+
 const colorPalette = {
-  background: darkest,
-  bodyText: brightest,
+  background: backgroundDark,
+  bodyText: figureLight,
   button: {
-    default: darkest,
-    focus: middle,
+    default: backgroundDark,
+    focus: backgroundLightest,
     shadow: light.reflection,
   },
-  controlPanel: darkerThanDarkest,
+  controlPanel: backgroundLight,
   disabledText: opacityForDisabledText,
-  displayBackground: pureBlack,
-  footerText: footerText,
+  footerText: figureDark,
   errorText: alert,
-  inputField: middle,
+  inputField: backgroundLight,
   linearLight: {
     backgroundColor: light.source,
     shadow: light.reflection,
   },
   link: {
     text: {
-      default: brightest,
+      default: figureLight,
       visited: visitedLink,
     },
     underline: {
-      default: brightest,
+      default: figureLight,
       visited: visitedLink,
     },
     background: {
