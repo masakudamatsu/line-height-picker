d61f168634385656d5af82abf3357e558c733b15
Unknown <m.kudamatsu@gmail.com>
2020-06-28 14:21:55 +0900

    Add each page's description to the meta tag #5

--- src/helper/metaData.js
+++ src/helper/metaData.js
@@ -8,9 +8,14 @@ export const pageTitle = {
 };
 
 export const pageDescription = {
-  home: '',
-  xHeight: '',
-  modularScale: '',
-  preview: '',
-  css: '',
+  home:
+    'A web application that helps web designers/developers pick the line-height value in CSS code to beautify paragraphs of text on webpages',
+  xHeight:
+    'Part of a web application in which the user can enter an x-height value that will be converted into the font-size value in CSS code',
+  modularScale:
+    'Part of a web application in which the user enters the ratio of x-height to line-height that will be converted into the line-height value in CSS code',
+  preview:
+    'Part of a web application which previews the paragraphs of text styled according to the user-selected values of x-height and its ratio to line-height',
+  css:
+    'Part of a web application in which the user can copy the CSS code for beautifying paragraphs of text, generated from the user-selected values of x-height and its ratio to line-height',
 };
